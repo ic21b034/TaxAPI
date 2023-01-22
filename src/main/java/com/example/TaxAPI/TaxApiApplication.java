68fe8985336b8246a5b7fa80f48525aca28cdb90
wahid <ic21b034@technikum-wien.at>
2023-01-22 21:01:44 +0100

    Initial commit

---
+++ src/main/java/com/example/TaxAPI/TaxApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.TaxAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxApiApplication.class, args);
+	}
+
+}
